e1fa80309c63ec7f68615a0daef89e6268d99156
Amalie S <amaliesmidth@gmail.com>
2018-06-25 18:50:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 var katzDeliLine = []
 
+
+
+
+function takeANumber(katzDeliLine){
+  
+}
+
+// takeANumber(katzDeliLine) => "Welcome. You're ticket number #1" [1]
+// takeANumber(katzDeliLine) => "Welcome. You're ticket number #2" [1,2]
+
+// nowServing(katzDeliLine) => [2]
+// nowServing(katzDeliLine) => []
+
+// takeANumber(katzDeliLine) => "Welcome. You're ticket number #3" [3]
+
+
+
 function takeANumber(katzDeliLine, newCustomer) {
   katzDeliLine.push(newCustomer)
   return `Welcome, ${newCustomer}. You are number ${katzDeliLine.length} in line.` 
